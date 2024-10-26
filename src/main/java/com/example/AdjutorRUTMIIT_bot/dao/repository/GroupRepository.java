a801ce172c0b9579da1888bf50f489c675bb786b
ZQR0 <yarikrech@gmail.com>
2024-10-26 16:54:42 +0300

    docstring to repos added

--- src/main/java/com/example/AdjutorRUTMIIT_bot/dao/repository/GroupRepository.java
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/dao/repository/GroupRepository.java
@@ -6,7 +6,27 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository {
+
+    /**
+     * Метод для поиска группы по имени группы
+     * @param groupName имя группы
+     * @author Yaroslav Rechkalov
+     * */
     Optional<GroupEntity> findByGroupName(String groupName);
+
+    /**
+     * Метод для поиска группы по Id пользователя
+     * @param creatorId Id создателя группы
+     * @author Yaroslav Rechkalov
+     * */
     Optional<GroupEntity> findByCreatorId(Integer creatorId);
+
+    /**
+     * Метод для получения вообще всех групп (в будущем будет добавлено ограничение по кол-ву групп,
+     * но уже в другом методе)
+     * */
     Optional<List<GroupEntity>> getAllGroups();
+
+    // TODO: реализовать данный метод Артёму, в файле GroupRepositoryImpl
+    //Optional<GroupEntity> safeDeleteByGroupName(String groupName);
 }
--- src/main/java/com/example/AdjutorRUTMIIT_bot/dao/repository/UserRepository.java
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/dao/repository/UserRepository.java
@@ -10,10 +10,30 @@ import java.util.Optional;
  * с таблицей пользователей в базе данных
  * */
 public interface UserRepository {
+
+    /**
+     * Метод репозитория для поиска пользователя по СНИЛС
+     * @param SNILS собственно сам СНИЛС
+     * @author Yaroslav Rechkalov
+     * */
     Optional<UserEntity> findBySNILS(String SNILS);
+
+    /**
+     * Метод для поиска пользователя по ФИО
+     * @param firstName Имя
+     * @param secondName Фамилия
+     * @param patronymic Отчество
+     * @author Yaroslav Rechkalov
+     * */
     Optional<UserEntity> findByFirstNameAndSecondNameAndPatronymic(String firstName,
                                                                    String secondName,
                                                                    String patronymic);
+
+    // TODO: реализовать данные методы Артёму, в файле UserRepositoryImpl
+//    Optional<UserEntity> safeDeleteByFSP(String firstName,
+//                                         String secondName,
+//                                         String patronymic);
+//    Optional<UserEntity> safeDeleteBySNILS(String SNILS);
 
 
 }
