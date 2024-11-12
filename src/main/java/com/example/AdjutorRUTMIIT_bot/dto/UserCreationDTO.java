5437c63e8d83b8c964a621c895257219cd698693
ZQR0 <yarikrech@gmail.com>
2024-11-13 00:10:06 +0300

    special dto

---
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/dto/UserCreationDTO.java
@@ -0,0 +1,43 @@
+package com.example.AdjutorRUTMIIT_bot.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UserCreationDTO {
+
+    private String firstName;
+    private String secondName;
+    private String patronymic;
+    private String SNILS;
+    private String phoneNumber;
+    private String role;
+
+    @JsonProperty(namespace = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @JsonProperty(namespace = "second_name")
+    public String getSecondName() {
+        return secondName;
+    }
+
+    @JsonProperty(namespace = "patronymic")
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    @JsonProperty(namespace = "SNILS")
+    public String getSNILS() {
+        return SNILS;
+    }
+
+    @JsonProperty(namespace = "phone_number")
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @JsonProperty(namespace = "role")
+    public String getRole() {
+        return role;
+    }
+}
