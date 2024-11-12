a3bb2931a994efb9ec3eba357b71e56bae78e28e
ZQR0 <yarikrech@gmail.com>
2024-11-13 00:09:29 +0300

    exception

---
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/exception/UniqueEntityAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.AdjutorRUTMIIT_bot.exception;
+
+public class UniqueEntityAlreadyExistsException extends Exception {
+    public UniqueEntityAlreadyExistsException(String message) {
+        super(message);
+    }
+}
