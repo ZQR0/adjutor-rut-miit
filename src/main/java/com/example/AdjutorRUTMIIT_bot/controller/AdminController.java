96081d45243b659fc40e3f9be1809dc366e92c8f
ZQR0 <yarikrech@gmail.com>
2024-11-13 00:08:31 +0300

    admin controller added

---
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/controller/AdminController.java
@@ -0,0 +1,32 @@
+package com.example.AdjutorRUTMIIT_bot.controller;
+
+import com.example.AdjutorRUTMIIT_bot.dto.UserCreationDTO;
+import com.example.AdjutorRUTMIIT_bot.dto.UserDTO;
+import com.example.AdjutorRUTMIIT_bot.exception.UniqueEntityAlreadyExistsException;
+import com.example.AdjutorRUTMIIT_bot.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/admin/v1/")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping(path = "create-user")
+    public ResponseEntity<UserDTO> createUserEndpoint(@RequestBody UserCreationDTO dto)
+        throws UniqueEntityAlreadyExistsException
+    {
+        return new ResponseEntity<>(
+                this.adminService.createUserByAdmin(dto),
+                HttpStatus.OK
+        );
+    }
+
+}
