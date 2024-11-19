aa40d502ed6b068c65129715f7facee98e395134
Dimdim786 <lvolk786@gmail.com>
2024-11-19 21:30:19 +0300

    wrote a snils check

--- src/main/java/com/example/AdjutorRUTMIIT_bot/utils/InputValidator.java
+++ src/main/java/com/example/AdjutorRUTMIIT_bot/utils/InputValidator.java
@@ -1,5 +1,8 @@
 package com.example.AdjutorRUTMIIT_bot.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class InputValidator {
     public static boolean check(String name_group) {
         String[] badWords = {"fuck", "cock", "dick"};
@@ -9,5 +12,17 @@ public class InputValidator {
             }
         }
         return true;
+    }
+    public static boolean checkName(String nameOfGroup) {
+        String regex = "^[А-Я]{3}-\\d{3}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(nameOfGroup);
+        return matcher.matches();
+    }
+    public static boolean checkSnils(String snils) {
+        String regex = "^\\d{3}-\\d{3}-\\d{3} \\d{2}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(snils);
+        return matcher.matches();
     }
 }
