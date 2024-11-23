b5bd0560ed500dfdf77998db5db2f032eb62ee47
Gosha-123 <egorgafarov086@gmail.com>
2024-11-23 22:04:19 +0300

    создание unit тестов

---
+++ src/test/java/com/example/AdjutorRUTMIIT_bot/utils/InputValidatorTests.java
@@ -0,0 +1,30 @@
+package com.example.AdjutorRUTMIIT_bot.utils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
+@SpringBootTest
+public class InputValidatorTests {
+
+    private static final String nameOfGroup = "УВП-111";
+    private static final String snils = "123-456-789 12";
+    private static final String nameOfGroupFalse = "фаврфы-123";
+    private static final String snilsFalse = "1231231237234";
+
+    @Test
+    void checkNameToRegexTest() {
+        assertThat(InputValidator.checkName(nameOfGroup)).isTrue();
+    }
+    @Test
+    void checkSnilsToRegexTest(){
+        assertThat(InputValidator.checkSnils(snils)).isTrue();
+    }
+    @Test
+    void checkNameFalseToRegexTest(){
+        assertThat(InputValidator.checkSnils(nameOfGroupFalse)).isFalse();
+    }
+    @Test
+    void checkSnilsFalseToRegexTest(){
+        assertThat(InputValidator.checkSnils(snilsFalse)).isFalse();
+    }
+}
