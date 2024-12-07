9876f341ed0de07fcecc9d239a3be457e1c86fc9
ZQR0 <yarikrech@gmail.com>
2024-12-07 16:03:19 +0300

    db structure changed

--- src/main/resources/database/version-1.0.0/data/default-users.sql
+++ src/main/resources/database/version-1.0.0/data/default-users.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset yaroslav:default-users-1
-INSERT INTO adjutor_schema.users_table (first_name, last_name, phone_number, patronymic, role, snils)
-VALUES ('Ivan', 'Ivanov', '8999999999', 'Ivanovich', 'admin', '123-456-789 12');
+INSERT INTO adjutor_schema.users_table (first_name, last_name, patronymic, role, snils)
+VALUES ('Ivan', 'Ivanov', 'Ivanovich', 'admin', '123-456-789 12');
--- src/main/resources/database/version-1.0.0/object/group.sql
+++ src/main/resources/database/version-1.0.0/object/group.sql
@@ -1,17 +1,15 @@
 --liquibase formatted sql
 
---changeset yaroslav:group-3
+--changeset yaroslav:group-4
 CREATE TABLE IF NOT EXISTS adjutor_schema.groups_table (
     group_id            INTEGER         NOT NULL        GENERATED ALWAYS AS IDENTITY,
     group_name          VARCHAR(100)    NOT NULL        UNIQUE,
     group_description   VARCHAR(100)    NOT NULL,
     join_link           VARCHAR(500)    NOT NULL,
-    is_deleted          BOOLEAN         DEFAULT FALSE,
+    is_deleted          BOOLEAN         NOT NULL        DEFAULT FALSE,
+    is_added_to_list    BOOLEAN         NOT NULL        DEFAULT FALSE,
     creation_date_time  TIMESTAMP       NOT NULL        DEFAULT CURRENT_TIMESTAMP,
     updating_date_time  TIMESTAMP       NOT NULL        DEFAULT CURRENT_TIMESTAMP
-    --creator_id          INTEGER         NOT NULL,
-    --members_ids         INTEGER[]       NOT NULL,
-    --CONSTRAINT fk_user  FOREIGN KEY (creator_id)        REFERENCES adjutor_schema.users_table(user_id)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS idx_groups_n1 ON adjutor_schema.groups_table (group_id);
@@ -27,4 +25,4 @@ $$ language 'plpgsql';
 
 
 -- updating trigger
-CREATE TRIGGER updating_date_trigger_groups_table BEFORE UPDATE ON adjutor_schema.groups_table FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
+--CREATE TRIGGER updating_date_trigger_groups_table BEFORE UPDATE ON adjutor_schema.groups_table FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
